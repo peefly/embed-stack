ba1456447ee284a9eb8c8adc4bd5a1cb5599ca76
Peefly <peefly@gmail.com>
2019-07-20 22:51:59 +0800

    feat: embed 1st ver.

--- src/components/Embed.js
+++ src/components/Embed.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { FaTimes } from 'react-icons/fa'
+import { Rnd } from "react-rnd";
+import { Button } from 'react-bootstrap'
+import './Embed.scss';
 
 export const Embed = (props) => {
   const {embed} = props;
@@ -6,8 +10,61 @@ export const Embed = (props) => {
   const createMarkup = (embedData) => {
     return {__html: embedData};
   };
-  
+
+  const backgroundStyle = {
+    //display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "solid 0px #aaa",
+    background: "#222",
+    
+  };
+
+  let controlPanelStyle = {
+    height: "32px",
+    width: "100%",
+    top: "-32px",
+    left: "-1px",
+    position: "absolute",
+    backgroundColor: "#222",
+    color: "#fff",
+    border: "solid 0px #aaa",
+    margin: "1px",
+    alignItems: "right",
+    textAlign: "right",
+  }
+  const [controlPanelVisible, setControlPanelVisible] = useState(false);
+
+  const showControlPanel = (e)=> {
+    setControlPanelVisible(true);
+    console.log("showControlPanel");
+  }
+  const hideControlPanel = (e)=> {
+    setControlPanelVisible(false);
+    console.log("hideControlPanel");
+  }
+
   return (
-    <div dangerouslySetInnerHTML={createMarkup(embed)} />
+      <Rnd
+        style={backgroundStyle}
+        default={{
+          x: 100, y: 100,
+          width: 960, height: 540
+        }}
+        lockAspectRatio
+        onMouseOver={showControlPanel}
+        onMouseOut={hideControlPanel}
+      >
+        { controlPanelVisible &&
+          <div style={controlPanelStyle}>
+            <Button variant="dark" size="sm"><FaTimes /></Button>
+          </div> 
+        }
+        
+        <div className="EmbedBackground full" dangerouslySetInnerHTML={createMarkup(embed)} />
+      </Rnd>
+    
+
+    
   );
 }
--- src/components/InputPanel.js
+++ src/components/InputPanel.js
@@ -2,7 +2,8 @@ import React, {useState} from 'react'
 import { Button, InputGroup, Form } from 'react-bootstrap';
 
 export const InputPanel = ({addHandler}) => {
-  const [embedInput, setEmbedInput] = useState("");
+  const defaultInput = "<iframe src='https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2FLeggyReki%2Fvideos%2F712018952592305%2F&show_text=0&width=560' style='border:none;overflow:hidden;width:100%;height:100%' scrolling='no' frameborder='0' allowTransparency='true' allowFullScreen='true'></iframe>";
+  const [embedInput, setEmbedInput] = useState(defaultInput);
   const onInputChange = (evt) => setEmbedInput(evt.target.value);
   const onAddClick = () => {
     addHandler(embedInput);
